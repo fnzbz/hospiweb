b68e428cd3d35188d7b6e754814a749491407871
fnzbz <dangerzsamp@gmail.com>
2018-07-27 17:49:50 +0300

    Update Database

--- novacdan_hospiweb.sql
+++ novacdan_hospiweb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 17, 2018 at 05:35 PM
+-- Generation Time: Jul 27, 2018 at 05:47 PM
 -- Server version: 5.6.39-log
 -- PHP Version: 5.6.30
 
@@ -55,6 +55,16 @@ CREATE TABLE `aditional_medic` (
   `lastModified` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_romanian_ci;
 
+--
+-- Dumping data for table `aditional_medic`
+--
+
+INSERT INTO `aditional_medic` (`accountID`, `spital`, `limbasec`, `specializare`, `program`, `cabinet`, `pret`, `reqStatus`, `lastModified`) VALUES
+(60, 'Spitalul Judetean &#34;Sf. Andrei&#34; Galati', 2, 'Boli infectioase', '12:00 - 17:00', 'str. ', '300', 1, 1532324129),
+(63, '', 1, 'Anestezie si terapie intensiva', '', '', '', 1, 1531836064),
+(74, '', 1, 'Alergologie si imunologie', '', '', '', 1, 1531895416);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `aditional_pacient`
@@ -75,6 +85,35 @@ CREATE TABLE `aditional_pacient` (
   `lastModified` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_romanian_ci;
 
+--
+-- Dumping data for table `aditional_pacient`
+--
+
+INSERT INTO `aditional_pacient` (`accountID`, `limbasec`, `greutate`, `inaltime`, `vaccinuri`, `oredormit`, `dependenta`, `exfizice`, `domiciliu`, `alergii`, `intoleranta`, `lastModified`) VALUES
+(60, 1, 0, 0, 1, 1, 4, 1, '', '', '', 1531855825),
+(63, 4, 80, 167, 1, 1, 2, 1, '', '', '', 1531633490),
+(74, 1, 0, 0, 1, 1, 1, 1, '', '', '', 1531856550),
+(79, 1, 76, 201, 1, 1, 1, 1, '', '', '', 1532634594);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `avatars`
+--
+
+CREATE TABLE `avatars` (
+  `id` int(11) NOT NULL,
+  `accountID` int(11) NOT NULL,
+  `avatarName` varchar(255) CHARACTER SET utf8 COLLATE utf8_romanian_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `avatars`
+--
+
+INSERT INTO `avatars` (`id`, `accountID`, `avatarName`) VALUES
+(12, 63, '635b55608b9595a.jpg'),
+(22, 60, '605b5755b645f57.jpg');
 
 -- --------------------------------------------------------
 
@@ -90,7 +129,6 @@ CREATE TABLE `consultatii_pacient` (
   `mention` text COLLATE utf8_romanian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_romanian_ci;
 
-
 -- --------------------------------------------------------
 
 --
@@ -105,6 +143,13 @@ CREATE TABLE `diagnostic_pacient` (
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_romanian_ci;
 
+--
+-- Dumping data for table `diagnostic_pacient`
+--
+
+INSERT INTO `diagnostic_pacient` (`id`, `accountID`, `text`, `medic`, `time`) VALUES
+(25, 63, 'Diagnostic', 'Novac Dan Andrei', '2018-07-26 19:36:55'),
+(27, 74, 'dada', 'Novac Dan Andrei', '2018-07-22 15:30:48');
 
 -- --------------------------------------------------------
 
@@ -138,6 +183,18 @@ CREATE TABLE `heartbeat_chart` (
   `timestamp` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_romanian_ci;
 
+--
+-- Dumping data for table `heartbeat_chart`
+--
+
+INSERT INTO `heartbeat_chart` (`keyID`, `accountCNP`, `sistola`, `diastola`, `pulse`, `day`, `timestamp`) VALUES
+(47, '2680711170410', 131, 80, 90, 1, 0),
+(48, '2680711170410', 131, 80, 68, 2, 1531240889),
+(50, '2680711170410', 80, 50, 80, 3, 1531337900),
+(51, '2680711170410', 195, 87, 70, 4, 1531567932),
+(52, '2680711170410', 180, 90, 60, 5, 1531632787),
+(55, '1310104408066', 120, 80, 90, 1, 0),
+(56, '1310104408066', 132, 76, 86, 2, 1531843814);
 
 -- --------------------------------------------------------
 
@@ -153,6 +210,13 @@ CREATE TABLE `medicperm` (
   `isAcc` tinyint(1) NOT NULL DEFAULT '0',
   `date` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_romanian_ci;
+
+--
+-- Dumping data for table `medicperm`
+--
+
+INSERT INTO `medicperm` (`id`, `pacientID`, `namePacient`, `medicID`, `isAcc`, `date`) VALUES
+(61, 79, 'Vrinceanu Radu', 60, 1, 1532634636),
 
 -- --------------------------------------------------------
 
@@ -183,6 +247,12 @@ CREATE TABLE `simptome_pacient` (
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_romanian_ci;
 
+--
+-- Dumping data for table `simptome_pacient`
+--
+
+INSERT INTO `simptome_pacient` (`id`, `accountID`, `text`, `medic`, `time`) VALUES
+(26, 63, 'Testez', 'Orzan Melania Carmen', '2018-07-15 07:33:13');
 
 -- --------------------------------------------------------
 
@@ -202,7 +272,14 @@ CREATE TABLE `tickets` (
   `status` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_romanian_ci;
 
+--
+-- Dumping data for table `tickets`
+--
 
+INSERT INTO `tickets` (`id`, `accountID`, `numeCreator`, `subiect`, `departament`, `urgenta`, `text`, `data`, `status`) VALUES
+(43, 63, 'Orzan Melania Carmen', 'Testez sistemu 2', 2, 3, 'dada', 1532173433, 0),
+(45, 63, 'Orzan Melania Carmen', 'Testez sistemul 4', 3, 1, 'Dadada', 1532173458, 0),
+(46, 79, 'Vrinceanu Radu', 'Dada', 1, 2, 'dadada', 1532635469, 0);
 
 -- --------------------------------------------------------
 
@@ -276,6 +353,16 @@ CREATE TABLE `utilizatori` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_romanian_ci;
 
 --
+-- Dumping data for table `utilizatori`
+--
+
+INSERT INTO `utilizatori` (`id`, `CNP`, `utilizator`, `mail`, `password`, `telefon`, `sex`, `sange`, `stare`, `nascut`, `judet`, `isMedic`, `confirmedEmail`, `lastLogin`, `isMod`) VALUES
+(60, '1310104408066', 'Novac Dan Andrei', 'castorulmc@gmail.com', '$2y$10$Iqcm1/TybR3Z0wQ6bTUnD.vi9KkxRij66zSkYgL2.nuC7yhv0SgAS', '0747617579', 1, 4, 1, -1230515064, 'Bucuresti', 1, 1, 1532700741, 1),
+(63, '2680711170410', 'Orzan Melania Carmen', 'mellynov@yahoo.com', '$2y$10$4dl52euFmc8dPRRVDCQ0c.XRW2gyeIcW4OeAR8YklNxKhDMFc/VGe', '0756131339', 2, 2, 1, -46576800, 'Galati', 0, 0, 1532670634, 0),
+(74, '1970301114544', 'Robert Colca', 'robert.mihai.colca@gmail.com', '$2y$10$F71jiKfb6nl8LKnYqWsRBOlBOKUmdBWeYSIi2MxruCmZoGzLRSzDW', '0763691414', 1, 3, 1, 857167200, 'Caras-Severin', 1, 0, 1531895409, 0),
+(79, '1950907431418', 'Vrinceanu Radu', 'dangerzsamp@gmail.com', '$2y$10$0JRIkRpsI0lGwd3tUVMSb.foLmRQ5XOzEmDezg.QdQrsIAAJD36B2', '0747617579', 1, 2, 2, 810421200, 'Bucuresti', 0, 0, 1532634666, 0);
+
+--
 -- Indexes for dumped tables
 --
 
@@ -290,6 +377,13 @@ ALTER TABLE `aditional_medic`
 --
 ALTER TABLE `aditional_pacient`
   ADD PRIMARY KEY (`accountID`);
+
+--
+-- Indexes for table `avatars`
+--
+ALTER TABLE `avatars`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `accountID` (`accountID`);
 
 --
 -- Indexes for table `consultatii_pacient`
@@ -384,76 +478,82 @@ ALTER TABLE `utilizatori`
 --
 
 --
+-- AUTO_INCREMENT for table `avatars`
+--
+ALTER TABLE `avatars`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
 -- AUTO_INCREMENT for table `consultatii_pacient`
 --
 ALTER TABLE `consultatii_pacient`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
 
 --
 -- AUTO_INCREMENT for table `diagnostic_pacient`
 --
 ALTER TABLE `diagnostic_pacient`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `email_confirm`
 --
 ALTER TABLE `email_confirm`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `heartbeat_chart`
 --
 ALTER TABLE `heartbeat_chart`
-  MODIFY `keyID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
+  MODIFY `keyID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 
 --
 -- AUTO_INCREMENT for table `medicperm`
 --
 ALTER TABLE `medicperm`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 
 --
 -- AUTO_INCREMENT for table `password_reset_req`
 --
 ALTER TABLE `password_reset_req`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `simptome_pacient`
 --
 ALTER TABLE `simptome_pacient`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `tickets`
 --
 ALTER TABLE `tickets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT for table `tickets_comments`
 --
 ALTER TABLE `tickets_comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- AUTO_INCREMENT for table `transplanturi_pacient`
 --
 ALTER TABLE `transplanturi_pacient`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `tratament_pacient`
 --
 ALTER TABLE `tratament_pacient`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT for table `utilizatori`
 --
 ALTER TABLE `utilizatori`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
 
 --
 -- Constraints for dumped tables
@@ -470,6 +570,12 @@ ALTER TABLE `aditional_medic`
 --
 ALTER TABLE `aditional_pacient`
   ADD CONSTRAINT `acc_id_aditional_pacient` FOREIGN KEY (`accountID`) REFERENCES `utilizatori` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `avatars`
+--
+ALTER TABLE `avatars`
+  ADD CONSTRAINT `profile_avatars` FOREIGN KEY (`accountID`) REFERENCES `utilizatori` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `consultatii_pacient`
